53b955ee190cecdda86baa0ee7b1285bf00d3182
H.-H. Peng <hsinspeng@gmail.com>
2022-02-07 14:08:08 +0800

    ✅ finish project.

---
+++ README.md
@@ -0,0 +1,41 @@
+<!-- Badge for License -->
+<div align="right">
+
+  [![](https://img.shields.io/github/license/Hsins-Lab/vue-video-browser.svg?style=flat-square)](./LICENSE)
+
+</div>
+
+<!-- Logo, Title and Description -->
+<div align="center">
+
+# Udemy Project: Vue Video Browser
+
+🧪 _The hands-on project from the Udemy course - ["Vue.js Essentials with Vuex and Vue Router"](https://www.udemy.com/course/vue-js-course/) by [Stephen Grider](https://www.udemy.com/user/sgslo/)._
+
+</div>
+
+## Features
+
+## Getting Started
+
+```bash
+# clone the repository
+$ git clone https://github.com/Hsins-Lab/vue-video-browser
+
+# install dependencies
+$ cd vue-video-browser
+$ yarn install
+
+# compiles and hot-reloads for development
+$ yarn serve
+
+# compiles and minifies for production
+$ yarn build
+
+# lints and fixes files
+$ yarn lint
+```
+
+## License
+
+Licensed under the MIT License, Copyright © 2019-present Hsins.
---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <SearchBar v-on:termChange="onTermChange"></SearchBar>
+    <div class="row">
+      <VideoDetail :video="selectedVideo" />
+      <VideoList @videoSelect="onVideoSelect" :videos="videos"></VideoList>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
+import VideoDetail from './components/VideoDetail';
+
+const API_KEY = 'AIzaSyB0ToJFPdt3Dp2stb8ynXqHkNe9U0ivB0c';
+
+export default {
+  name: 'App',
+  components: {
+    SearchBar,
+    VideoList,
+    VideoDetail,
+  },
+  data() {
+    return { videos: [], selectedVideo: null };
+  },
+  methods: {
+    onVideoSelect(video) {
+      this.selectedVideo = video;
+    },
+    onTermChange(searchTerm) {
+      axios
+        .get('https://www.googleapis.com/youtube/v3/search', {
+          params: {
+            key: API_KEY,
+            type: 'video',
+            part: 'snippet',
+            q: searchTerm,
+          },
+        })
+        .then((response) => {
+          this.videos = response.data.items;
+        });
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
